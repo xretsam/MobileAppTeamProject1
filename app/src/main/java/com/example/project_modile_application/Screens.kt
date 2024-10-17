740a0023eb28453da8369dc21c0d9b70a679294e
Adilkhan Yerbolat <dilhanmq@gmail.com>
2024-10-18 00:53:31 +0500

    fix: small issue fix

--- app/src/main/java/com/example/project_modile_application/Screens.kt
+++ app/src/main/java/com/example/project_modile_application/Screens.kt
@@ -5,12 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.project_modile_application.ui.screen.home.Home as HomeScreen
 
 
 @Composable
 fun Home() {
-    HomeScreen()
+    com.example.project_modile_application.ui.screen.home.Home()
 }
 
 
