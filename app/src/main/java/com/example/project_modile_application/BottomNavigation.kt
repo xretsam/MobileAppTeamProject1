e75fd3a89e7282e05125d3b287c960010110393d
color.ui <eskendir16092004@gmail.com>
2024-10-17 20:25:41 +0500

    Added shadow dor navigationBar , changed colors of containers

--- app/src/main/java/com/example/project_modile_application/BottomNavigation.kt
+++ app/src/main/java/com/example/project_modile_application/BottomNavigation.kt
@@ -1,18 +1,30 @@
 package com.example.project_modile_application
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
+
+@Preview(showBackground = true)
 @Composable
 fun BottomNavigation() {
     val navController = rememberNavController()
@@ -24,7 +36,12 @@ fun BottomNavigation() {
 
     Scaffold(
         bottomBar = {
-            NavigationBar {
+            NavigationBar(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+                containerColor = Color.White
+            )
+            {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route
 
@@ -32,32 +49,64 @@ fun BottomNavigation() {
                     NavigationBarItem(
                         selected = currentRoute == screen.route,
                         icon = {
-                            Icon(
-                                painter = when (screen) {
-                                    is Screen.Home -> painterResource(id = R.drawable.iconhome)
-                                    is Screen.Search -> painterResource(id = R.drawable.iconsearch)
-                                    is Screen.Profile -> painterResource(id = R.drawable.iconperson)
-                                },
-                                contentDescription = screen.route
-                            )
+                            when (screen) {
+                                is Screen.Home -> Icon(
+                                    painter = painterResource(id = R.drawable.iconhome),
+                                    contentDescription = "Home",
+                                    modifier = Modifier
+                                        .size(24.dp)
+
+                                )
+
+                                is Screen.Search -> Icon(
+                                    painter = painterResource(id = R.drawable.iconsearch),
+                                    contentDescription = "Search",
+                                    modifier = Modifier
+                                        .size(24.dp)
+                                )
+
+                                is Screen.Profile -> Icon(
+                                    painter = painterResource(id = R.drawable.iconperson),
+                                    contentDescription = "Profile",
+                                    modifier = Modifier
+                                        .size(24.dp)
+                                )
+
+                                Screen.Store -> TODO()
+                            }
                         },
                         onClick = {
-                            if (currentRoute != screen.route) {
-                                navController.navigate(screen.route) {
-                                    popUpTo(navController.graph.startDestinationId) {
-                                        saveState = true
-                                    }
-                                    launchSingleTop = true
-                                    restoreState = true
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
                                 }
+                                launchSingleTop = true
+                                restoreState = false
                             }
-                        }
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            unselectedIconColor = Color(0xFF272727),
+                            selectedIconColor = Color(0xFF3D3BFF),
+                            unselectedTextColor = Color(0xFF272727),
+                            selectedTextColor = Color(0xFF3D3BFF),
+                            indicatorColor = Color.White
+                        ),
                     )
                 }
             }
         }
     ) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RectangleShape,
+                    ambientColor = Color(0xFF3D3BFF),
+                    spotColor = Color(0xFF3D3BFF)
+                )
+                .background(color = Color.White)
+        ) {
             NavigationGraph(navController = navController)
         }
     }
